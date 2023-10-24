dc74d3a08fd86d28948fdc33c937f31014dc6b5e
Olga Kostyaeva <o-kostyaeva@yandex.ru>
2023-10-24 12:53:01 +0300

    final

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -11,7 +11,16 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+        long actual = service.findMax(incomesInBillions);
 
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxinmidle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 60, 11, 11, 12};
+        long expected = 60;
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
